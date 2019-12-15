87829e6dbdd3f6097a04ba6c462cc02bf20cf240
cccodaisy <58411126+cccodaisy@users.noreply.github.com>
2019-12-15 23:38:05 +0900

    #1.6 Saving To Dos - flush

--- App.js
+++ App.js
@@ -9,29 +9,39 @@ import {
   Platform,
   ScrollView
 } from 'react-native';
+import { AppLoading } from "expo";
 import ToDo from "./ToDo";
+import uuidv1 from "uuid/v1"
 
 const { height, width } = Dimensions.get("window");/* window 기준으로 간격을 정해주기 위해 */
 
 export default class App extends React.Component{
   state = {
-    newToDo: ""
+    newToDo: "",
+    loadedToDos: false
+  };
+  componentDidMount = () => {
+    this._loadToDos();
   };
   render() {
-    const { newToDo } =this.state;
+    const { newToDo, loadedToDos } = this.state;
+    if (!loadedToDos) {
+      return <AppLoading />;
+    }
     return (
       <View style={styles.container}>
         <StatusBar barStyle="light-content" />
         <Text style={styles.title}>To Do</Text>
         <View style={styles.card}>
           <TextInput 
-            style={styles.input} 
-            placeholder={"New To Do"} 
-            value={newToDo} 
-            onChangeText={this._controlNewToDo} 
-            placeholderTextColor={"#999"} 
+            style={styles.input}
+            placeholder={"New To Do"}
+            value={newToDo}
+            onChangeText={this._crontollNewToDo}
+            placeholderTextColor={"#999"}
             returnKeyType={"done"}
             autoCorrect={false}
+            onSubmitEditing={this._addToDo}
           />
           <ScrollView contentContainerStyle={styles.toDos}>
             <ToDo text={"Hello I'm a To Do"}/>
@@ -40,11 +50,41 @@ export default class App extends React.Component{
       </View>
     );
   }
-  _controlNewToDo = text => {
+  _crontollNewToDo = text => {
     this.setState({
       newToDo: text
-    })
-  }
+    });
+  };
+  _loadToDos = () => {
+    this.setState({
+      loadedToDos: true
+    });
+  };
+  _addToDo = () => {
+    const { newToDo } = this.state;
+    if (newToDo !== "") {
+      this.setState(prevState => {
+        const ID = uuidv1();
+        const newToDoObject = {
+          [ID]: {
+            id: ID,
+            isCompleted: false,
+            text: newToDo,
+            createdAt: Date.now()
+          }
+        };
+        const newState = {
+          ...prevState,
+          newToDo: "", /* input 비움 */
+          toDos: {
+            ...prevState.toDos,
+            ...newToDoObject
+          }
+        };
+        return { ...newState };
+      });
+    };
+  };
 }
 
 const styles = StyleSheet.create({
